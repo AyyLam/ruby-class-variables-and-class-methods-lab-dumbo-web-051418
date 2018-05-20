d2bd04614a6e12f22d729c049c87f0e239b5efd0
Anthony Lam <al3101@nyu.edu>
2018-05-20 00:35:41 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = [] 
+
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre 
+    @@count << self 
+  end 
+  
+  def self.count 
+    @@count.size 
+  end 
+  
+  def self.artists
+    @@count.select {|song| song.artist}
+  end
+  
+  def self.assert_generates
+    @@count.select {|song| song.genres}
+  end
+
+end 
+  
