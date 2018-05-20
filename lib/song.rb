c227576657af6b53d2a008ad8c0fc529824a1d8e
Anthony Lam <al3101@nyu.edu>
2018-05-20 00:46:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   
   def self.genre_count
     genre_count = {} 
-    @@genres.each do |genre| 
+    @@genres.collect do |genre| 
       if genre_count[genre]
         genre_count[genre] += 1 
       else
